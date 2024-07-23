8180efafc97e1d34013097b067ad24e5b03d1669
Daniel Mursa <daniel.mursa@energee3.com>
2024-07-23 12:25:38 +0200

    Create admin and migrations

--- hotel/admin.py
+++ hotel/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import City, Hotel
 
-# Register your models here.
+@admin.register(City)
+class CityAdmin(admin.ModelAdmin):
+    list_display = ('code', 'name')  
+    list_filter = ('name',)          
+    search_fields = ('code', 'name') 
+    ordering = ('name',)            
+
+@admin.register(Hotel)
+class HotelAdmin(admin.ModelAdmin):
+    list_display = ('code', 'name', 'city')  
+    list_filter = ('city',)                  
+    search_fields = ('code', 'name', 'city__name') 
+    ordering = ('name',)                    
